b1584dab25093324fc4b8ef932d86d82d3c9206f
Alex Kucherenko <alexiko86@gmail.com>
2018-03-17 00:15:50 +0200

    Saving of access token on success login

--- app/src/main/java/estimator/kissteam/com/estimatorclient/LoginActivity.kt
+++ app/src/main/java/estimator/kissteam/com/estimatorclient/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import estimator.kissteam.com.estimatorclient.retrofit.auth.GetAccessTokenGateway
+import estimator.kissteam.com.estimatorclient.retrofit.auth.OauthRepository
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -25,7 +27,13 @@ class LoginActivity : AppCompatActivity() {
                         ).execute())
                     }
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe()
+                    .subscribe({ gatewayResult ->
+                        OauthRepository().saveAccessToken(gatewayResult.result?.accessToken)
+                    }, {
+                        Toast
+                                .makeText(LoginActivity@ this, "Error during authorization", Toast.LENGTH_LONG)
+                                .show()
+                    })
         })
     }
 }

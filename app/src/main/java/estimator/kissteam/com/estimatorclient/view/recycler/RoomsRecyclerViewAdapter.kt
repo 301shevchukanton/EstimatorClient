309ab81973e304d26cd690454f521d56ef0fe0be
MyrraWey <muravyovdmitr@gmail.com>
2018-03-17 15:34:24 +0200

    add estimated rooms

--- app/src/main/java/estimator/kissteam/com/estimatorclient/view/recycler/RoomsRecyclerViewAdapter.kt
+++ app/src/main/java/estimator/kissteam/com/estimatorclient/view/recycler/RoomsRecyclerViewAdapter.kt
@@ -2,9 +2,7 @@ package estimator.kissteam.com.estimatorclient.view.recycler
 
 import android.arch.lifecycle.MutableLiveData
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import estimator.kissteam.com.estimatorclient.R
 import estimator.kissteam.com.estimatorclient.dal.entities.Room
 import estimator.kissteam.com.estimatorclient.view.RoomItemView
 
@@ -13,27 +11,23 @@ import estimator.kissteam.com.estimatorclient.view.RoomItemView
  */
 
 class RoomsRecyclerViewAdapter(val myDataset: MutableList<Room>) :
-        RecyclerView.Adapter<RoomsRecyclerViewAdapter.ViewHolder>() {
-    val clickedRoom: MutableLiveData<Room> = MutableLiveData()
+		RecyclerView.Adapter<RoomsRecyclerViewAdapter.ViewHolder>() {
+	val clickedRoom: MutableLiveData<Room> = MutableLiveData()
 
-    class ViewHolder(val taskItemView: RoomItemView) : RecyclerView.ViewHolder(taskItemView)
+	class ViewHolder(val taskItemView: RoomItemView) : RecyclerView.ViewHolder(taskItemView)
 
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): RoomsRecyclerViewAdapter.ViewHolder {
-        val taskItemView = RoomItemView(parent.context)
-        return ViewHolder(taskItemView)
-    }
+	override fun onCreateViewHolder(parent: ViewGroup,
+									viewType: Int): RoomsRecyclerViewAdapter.ViewHolder {
+		val taskItemView = RoomItemView(parent.context)
+		return ViewHolder(taskItemView)
+	}
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.taskItemView.setRoom(myDataset[position])
-        holder.taskItemView.setOnClickListener {
-            clickedRoom.value = myDataset[position]
-        }
-    }
+	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+		holder.taskItemView.setRoom(myDataset[position])
+		holder.taskItemView.setOnClickListener {
+			clickedRoom.value = myDataset[position]
+		}
+	}
 
-    override fun getItemCount() = myDataset.size
-
-    fun addIssue(room: Room) {
-        myDataset.add(room)
-    }
+	override fun getItemCount() = myDataset.size
 }
